1c2d75679325b5c3d8e10c31e5744f10fe7fd181
Justin Shaw <easyworkemail@gmail.com>
2019-04-24 12:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function logShout(string) {
   console.log(string) // string returns a string variable 'hello'
   console.log(string.toUpperCase()) // which is convereted to upper via the toUpperCase() method which returns a string variable
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
